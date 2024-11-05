a632a3bdf688146f578a8f56fd9589c3ccab1006
Andy Hsu <andy@start2.cloud>
2024-11-05 16:55:53 +0800

    Change receive queue name

--- receive.py
+++ receive.py
@@ -2,6 +2,7 @@
 import time
 import random
 import pika
+import json
 
 # Default test host in GTM is 172.16.1.57
 # Web UI http://172.16.1.57:15672/#/
@@ -39,12 +40,29 @@ def callback(ch, method, properties, body):
     # body = b'Hello Word!!!Sun Jul 28 16:12:36 2024'
     # body.decode() = Hello Word!!!Sun Jul 28 16:12:36 2024
     print(" [!] Received %r" % (body.decode()))
-    # [x] Received 'Hello Word!!!Sun Jul 28 16:24:58 2024'
-    
-    ## Do something
-    print(" >>>>> Do something for " + body.decode())
-    time.sleep(2)
 
+    try:
+        # Attempt to decode the message as JSON
+        message = json.loads(body.decode('utf-8'))
+        print(" [!] Received JSON message: %r" % message)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except json.JSONDecodeError:
+        # If it's not valid JSON, print the raw message
+        print(" [!] Received non-JSON message: %r" % body.decode())
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+   
+    """
+    print(" >>>>> Do something for " + body.decode())
+    if body.decode() == 'Andy test':
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        print(" [O] Done \n")
+    else:
+        ch.basic_reject(delivery_tag = method.delivery_tag, requeue=False)
+        print(" [O] Done \n")
+    """
+    # time.sleep(1)
+
+    """
     if random.random() < 0.5:
         #如果正確處理，回傳delivery_tag，queue 會被移除
         ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -53,7 +71,7 @@ def callback(ch, method, properties, body):
         #如果處理異常，回傳 reject，資料會被丟到 dead queue
         ch.basic_reject(delivery_tag = method.delivery_tag, requeue=False)
         print(" [X] Rejected, send to [giant.dead] \n")
-
+    """
 
 
 while True:
@@ -61,13 +79,13 @@ while True:
         # create connection
         connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
         channel = connection.channel()
-        queue_name = 'giant.direct.queue-1'
-        
+        # queue_name = 'giant.direct.queue-3'
+        queue_name = 'FromAIP2SAP_DN_TOAIP2_WMS'
         # Check queue
         channel.queue_declare(queue_name, durable = True,
             arguments={
-                "x-dead-letter-exchange" : "giant.dead",
-                "x-queue-type" : "quorum"
+                #"x-dead-letter-exchange" : "giant.dead",
+                #"x-queue-type" : "quorum"
             #  "x-dead-letter-routing-key" : "dl", # if not specified, queue's routing-key is used
             }
         )
@@ -84,10 +102,13 @@ while True:
     
     # Don't recover if connection was closed by broker
     except pika.exceptions.ConnectionClosedByBroker:
+        print(pika.exceptions.ConnectionClosedByBroker)
         break
     # Don't recover on channel errors
     except pika.exceptions.AMQPChannelError:
+        print(pika.exceptions.AMQPChannelError)
         break
     # Recover on all other connection errors
     except pika.exceptions.AMQPConnectionError:
+        print(pika.exceptions.AMQPConnectionError)
         continue
